9c55e2c891e45fd10f9a7cfe020f2be2e07e94d1
WANG Kai <wangkai@godeepsite.com>
2022-04-22 12:17:43 +0000

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='document-insighter',
-    version='0.0.1',
+    version='0.0.2',
     author='Wang Kai',
     author_email='wangkai@godeepsite.com',
     description='Document Insighter Python Client',
